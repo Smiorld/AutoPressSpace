a017325d5bee376834d42d821a4bc1ec359bf3b6
Julius <39952090+Smiorld@users.noreply.github.com>
2022-09-27 23:06:36 +0100

    Update pressSpace.py

--- pressSpace.py
+++ pressSpace.py
@@ -1,15 +1,19 @@
 import pyautogui
 from pynput import keyboard
 
-# version 1.1
+# version 1.2
 def on_start_key(key):
     global startkey
-    if startkey == False and key != keyboard.Key.space and key != keyboard.Key.esc:
-        startkey=key
-        print('开始键设置为',key,'，按下开始键后空格将被自动按下，手动按一次空格键即可取消。退出程序直接右上角叉掉。')
+    if startkey == False:
+        if key != keyboard.Key.space and key != keyboard.Key.esc and key != keyboard.Key.enter and key != keyboard.Key.backspace and key != keyboard.Key.ctrl_l and key != keyboard.Key.ctrl_r and key != keyboard.Key.cmd and key != keyboard.Key.cmd_r and key != keyboard.Key.alt_l and key!= keyboard.Key.alt_gr:
+            startkey=key
+            print('开始键设置为',key,'，按下开始键后空格将被自动按下，手动按一次空格键即可取消。退出程序直接右上角叉掉。')
+        else:
+            print('开始键不能为空格，esc，ctrl，win，alt，enter，backspace,请重新设置')
     elif key == startkey :
         pyautogui.keyDown('space')
 
+ 
 
 startkey= False
 print('请按下您想要设置的本程序启动键')
